22435a90d89de2233a45b30dd99e03d723c211fb
ICT Vision <ictvision@localhost.localdomain>
2017-11-18 13:35:06 +0500

    Api portion

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'Api\UserController@login');
+
+//Route::post('details', 'Api\UserController@details');
+
+Route::group(['middleware' => 'auth:api'], function(){
+
+	Route::get('details', 'Api\UserController@details');
+	Route::get('attendance','Api\UserController@attendance');
+	Route::post('attendance-create','Api\UserController@attendance_create');
+	Route::post('student-classwise','Api\UserController@student_classwise');
+	Route::post('/attendance-view','Api\UserController@attendance_view');
+
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
