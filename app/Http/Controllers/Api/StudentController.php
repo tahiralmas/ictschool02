bbf42738b5b5e57735bb53410bd0b7f6c54ab1be
ICT Vision <ictvision@localhost.localdomain>
2017-11-21 16:35:12 +0500

    update api

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -32,59 +32,74 @@ class StudentController extends Controller
     }
    public $successStatus = 200;
 
-			/**
-		     * student_classwise api
-		     *
-		     * @return \Illuminate\Http\Response
-		     */
-		    public function student_classwise($class_level,$section,$shift,$session)
-		    {
-				
-					  $students = DB::table('Student')
-					  ->join('Class', 'Student.class', '=', 'Class.code')
-					  ->select('Student.id', 'Student.regiNo', 'Student.rollNo', 'Student.firstName', 'Student.middleName', 'Student.lastName', 'Student.fatherName', 'Student.motherName', 'Student.fatherCellNo', 'Student.motherCellNo', 'Student.localGuardianCell',
-					  'Class.Name as class', 'Student.presentAddress', 'Student.gender', 'Student.religion')
-					  //->where('isActive', '=', 'Yes')
-					  ->where('class',$class_level)
-					  ->where('section',$section)
-					  ->where('shift',$shift)
-					  ->where('session',trim($session))
-					  ->get();
-					  if(count($students)<1)
-					  {
-					     return response()->json(['error'=>'No Students Found!'], 401);
+	/**
+	 * student_classwise api
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function student_classwise($class_level,$section,$shift,$session)
+	{
+		  $students = DB::table('Student')
+		  ->join('Class', 'Student.class', '=', 'Class.code')
+		  ->select('Student.id', 'Student.regiNo', 'Student.rollNo', 'Student.firstName', 'Student.middleName', 'Student.lastName', 'Student.fatherName', 'Student.motherName', 'Student.fatherCellNo', 'Student.motherCellNo', 'Student.localGuardianCell',
+		  'Class.Name as class', 'Student.presentAddress', 'Student.gender', 'Student.religion')
+		  ->where('class',$class_level)
+		  ->where('section',$section)
+		  ->where('shift',$shift)
+		  ->where('session',trim($session))
+		  ->get();
+		  if(count($students)<1)
+		  {
+		     return response()->json(['error'=>'No Students Found!'], 401);
+		  }
+		  else {
+			  return response()->json(['students' => $students]);
+		  }
+	}
+    public function getstudent($student_id)
+    {
+         $student = Student::find($student_id);
+        if(!is_null($student) && $student->count()>0){
+           return response()->json(['studnet'=>$student]);
+        }else{
+        return response()->json(['error'=>'Student Not Found'], 401);
+       }
+    }
 
-					  }
-					  else {
-						  //$formdata = new formfoo;
-						 /* $formdata->class=Input::get('class');
-						  $formdata->section=Input::get('section');
-						  $formdata->shift=Input::get('shift');
-						  $formdata->session=trim(Input::get('session'));*/
-						  //return View::Make("app.studentList", compact('students','classes','formdata'));
-						  //return View("app.studentList", compact('students','classes','formdata'));
-						  return response()->json(['students' => $students]);
-					  }
-				  }
-
-		    public function getstudent($student_id)
-		    {
-                 $student = Student::find($student_id);
-                if(!is_null($student) && $student->count()>0){
-
-                   return response()->json(['studnet'=>$student]);
-		        }else{
-		        return response()->json(['error'=>'Student Not Found'], 401);
-	           }
-
-		    }
-
-		    public function update_student($student_id)
-		    {
-		    	 return response()->json(['student'=>$student_id]);
-		    }
-
-		    
+	public function update_student($student_id)
+	{
+		return response()->json(['student'=>$student_id]);
+		$rules=[
+		'fname' => 'required',
+		'lname' => 'required',
+		'gender' => 'required',
+		'session' => 'required',
+		'class' => 'required',
+		'section' => 'required',
+		'shift' => 'required',
+		'presentAddress' => 'required',
+		'parmanentAddress' => 'required'
+		];
+		$validator = \Validator::make(Input::all(), $rules);
+		if ($validator->fails())
+		{
+            return response()->json(['error'=>'Please Fill the Required Field'], 401);
+		}
+		else{
+			$student = Student::find($student_id);
+			$student->firstName= Input::get('fname');
+			$student->lastName= Input::get('lname');
+			$student->gender= Input::get('gender');
+			$student->session= trim(Input::get('session'));
+			$student->class= Input::get('class');
+			$student->section= Input::get('section');
+			$student->group= Input::get('group');
+			$student->presentAddress= Input::get('presentAddress');
+			$student->parmanentAddress= Input::get('parmanentAddress');
+			$student->save();
+			return response()->json(["success","Student Updated Succesfully."]);
+		}
+	}   
 }
 
 
