9185129df78551f05cb1d4590d909baba5d0916f
ICT Vision <ictvision@localhost.localdomain>
2018-02-21 14:32:49 +0500

    add column in migration

--- database/migrations/2018_02_21_092739_add_discount_id_to_Student.php
+++ database/migrations/2018_02_21_092739_add_discount_id_to_Student.php
@@ -14,7 +14,7 @@ class AddDiscountIdToStudent extends Migration
     public function up()
     {
         //
-         Schema::table('users', function($table)
+         Schema::table('Student', function($table)
       {
 	  $table->integer('discount_id');
       });
